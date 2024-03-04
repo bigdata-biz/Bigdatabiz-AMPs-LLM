f696873dc8a6a9c1736f732a2deae861a0453002
bigdata-biz <156773877+bigdata-biz@users.noreply.github.com>
2024-03-04 15:26:26 +0900

    Update load_models.sh
    test

--- 2_job-load-models/load_models.sh
+++ 2_job-load-models/load_models.sh
@@ -4,7 +4,7 @@ mkdir models
 cd models
 
 # Copy pre downloaded models
-cp -r /root/hyeongyu/models/eng-sample/* .
+cp -r /data/cdsw-sample/models/eng-sample/* .
 
 # Decomp & rm tar files
 tar -xvf ./embedding-model.tar
